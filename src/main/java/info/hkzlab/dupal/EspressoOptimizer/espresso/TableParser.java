b8bb40ecc051947178bfe251b6e253fdefb6dc5f
Fabio Battaglia <tabaglio@posteo.net>
2020-09-21 16:47:11 +0200

    Begin adding code to read the table from file

--- src/main/java/info/hkzlab/dupal/EspressoOptimizer/espresso/TableParser.java
+++ src/main/java/info/hkzlab/dupal/EspressoOptimizer/espresso/TableParser.java
@@ -1,5 +1,18 @@
 package info.hkzlab.dupal.EspressoOptimizer.espresso;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+
 public class TableParser {
-    
+    private TableParser() {
+    };
+
+    public static EspressoTable readTableFromBuffer(BufferedReader bufr) throws IOException {
+        String line;
+        while((line = bufr.readLine()) != null) {
+
+        }
+        
+        return null;
+    }
 }
