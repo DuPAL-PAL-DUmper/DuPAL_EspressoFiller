cd09c674ee90a6bd9227e29fd72a38f5b953d0c2
Fabio Battaglia <tabaglio@posteo.net>
2020-09-21 11:24:32 +0200

    Empty application

---
+++ src/main/java/info/hkzlab/dupal/EspressoOptimizer/App.java
@@ -0,0 +1,13 @@
+package info.hkzlab.dupal.EspressoOptimizer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
