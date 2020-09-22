a701cabe3937dbca863583b2594a70da5e8bb63c
Fabio Battaglia <tabaglio@posteo.net>
2020-09-22 07:53:45 +0200

    Begin add parsing for command line options

--- src/main/java/info/hkzlab/dupal/EspressoOptimizer/App.java
+++ src/main/java/info/hkzlab/dupal/EspressoOptimizer/App.java
@@ -1,13 +1,18 @@
 package info.hkzlab.dupal.EspressoOptimizer;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
+import org.slf4j.*;
+
+public class App {
+    private final static Logger logger = LoggerFactory.getLogger(App.class);
+    private final static String version = App.class.getPackage().getImplementationVersion();
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        if (args.length < 2) {
+            logger.error("Wrong number of arguments passed.\n"
+                    + "espresso_optimizer <input_file> <output_file>\n");
+
+            return;
+        }
     }
 }
